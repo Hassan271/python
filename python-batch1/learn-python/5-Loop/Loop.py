7438f69664a71e2466280c39e4983e1c872d84ba
hassan271 <hassanfaroq2@gmail.com>
2024-04-22 19:58:11 +0500

    Commit

--- python-batch1/learn-python/5-Loop/Loop.py
+++ python-batch1/learn-python/5-Loop/Loop.py
@@ -62,3 +62,77 @@ print("even",evenSum, "odd", oddSum )
 #     print("I will install this package on globally in your system!")
 
 # print(sys.argv) # iterative data type list[str] 0=filename **value user define
+
+
+
+
+# For Loop ==============================================
+
+# nums=[1,2,3,4,5,6,7,8,9,10]
+# for item in nums:
+#     print("print Numbers: ", item)
+
+
+# for j in range(0,20,4):
+#     print("for Loop Range: ", j)
+
+
+# Alphabet=["a","b","c","d","e"]
+# for k in Alphabet:
+#     if k == "d":
+#         print("print Alphabet: ", k[i])
+#         break
+    
+
+
+# nums = [10,11,30,13,50,19,70,80]
+# def multi(item):
+#     return item 
+# newList = list(filter(multi, nums))
+# print(newList)
+
+
+# def multi(item):
+#     if item % 2 == 0:
+#       return item * 2
+#     else:
+#         return item
+# newList = list(map(multi, nums))
+# print(newList)
+
+
+# userNames = ["Naveed", "john", "Alice", "Ali", "Umar", "Umair"]
+# def changeUserNames(item):
+#     print(item)
+#     return f"Hello {item}"
+# newNames = list(map(changeUserNames,userNames))
+
+# print(newNames)  
+
+
+# for index, item in  enumerate(userNames):
+#     print("loop runing", item, index)
+#     if item == "Ali":
+#         print("Search is found", item ,index ) 
+#         break
+
+# for item in  range(0, len(userNames)):
+#     print("loop runing", item)
+#     if userNames[item] == "Ali":
+#         print("Search is found", userNames[item] ,item ) 
+#         break
+    
+
+# nums = [10,20,30,40,50,60,70,80]
+# i = 0
+# while i < len(nums):
+#     print ("Hello world - while", nums[i])
+#     i = i + 1
+# write a search program, search the name ali from the list userName if ali found please stop the loop, print ali
+
+    
+# output  = list(range(10,50,2))
+# print("output",output)
+
+# for j in range(0,10,4):
+#     print("for loop range j", j)
--- python-batch1/learn-python/5-Loop/README.md
+++ python-batch1/learn-python/5-Loop/README.md
@@ -146,3 +146,33 @@ print("First Argument:", arguments[1])
 
 You can find various examples in this guide demonstrating how to use while loops, for loops, control statements, the input function, and command line arguments in Python.
 
+
+
+## #=============================================
+
+# Class Topics
+
+## Python - loops
+```
+list = [2,30,30,50]
+for i in list:
+    print(i)
+
+
+list = [2,30,30,50]
+for i,item in enumerate(list, start=10):
+    print(f"{i},{item}")
+
+
+    for i in range(1,10,2):
+    for j in range(1,10,2):
+        print(i,j)
+
+list3 = [2,30,30,50]
+list2 = [203,3023,3023,32450]
+def sumCal(i):
+    print(i)
+    return i * 2
+output = list(filter(sumCal, list3))
+print(output)
+```
